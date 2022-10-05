c9a37cc5e0587c14f9f7d2ca112349b92e37038d
nicoSoria83 <sorianicolas311@gmail.com>
2022-10-05 12:41:19 -0300

    se suman pequeños cambios

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,10 @@
-const botonToast = document.getElementById("mostrarBtnToast")
-botonToast.onclick = mostrarToast
+const botonToast = document.getElementById("ostrarBtnToast");
 
+botonToast.onclick = mostrarToast;
 function mostrarToast() {
-	Toastify({
- 	text: "This is a toast",
-  	duration: 3000,
-  	close: true,
-  	gravity: "bottom", // `top` or `bottom`
-  	position: "right", // `left`, `center` or `right`
-  	stopOnFocus: true, // Prevents dismissing of toast on hover
-  	style: {
-    background: "linear-gradient(to right, #00b09b, #96c93d)",
-  	},
-  onClick: function(){} // Callback after click
-}).showToast();
+  Toastify({
+    text: "Mostrar toast",
+    duration: 3000,
+    close: true,
+  }).showToast();
 }
